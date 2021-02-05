0662b3e7bc6999c761c2c920e29ea13044f17db4
Diogo Silva <diogovicente10@hotmail.com>
2021-02-05 10:08:20 +0000

    first commit

--- app.py
+++ app.py
@@ -34,22 +34,23 @@ def respond():
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
      if text == "/photo":
-     try:
-        # clear the message we got from any non alphabets
-        text = re.sub(r"\W", "_", text)
-        # create the api link for the avatar based on http://avatars.adorable.io/
-        url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-        # reply with a photo to the name the user sent,
-        # note that you can send photos by url and telegram will fetch it for you
-        bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-     except Exception:
-        # if things went wrong
-        bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+        try:
+            # clear the message we got from any non alphabets
+            text = re.sub(r"\W", "_", text)
+            # create the api link for the avatar based on http://avatars.adorable.io/
+            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+            # reply with a photo to the name the user sent,
+            # note that you can send photos by url and telegram will fetch it for you
+            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+        except Exception:
+            # if things went wrong
+            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+        return 'ok'
 
      else if text == "/echo" :
         update.message.reply_text(update.message.text)
            
-   return 'ok'
+   
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
