2c0ecffb3d853e86ffe4c4134f45bc7d0030c16c
Diogo Silva <diogovicente10@hotmail.com>
2021-02-05 11:05:49 +0000

    first commit

--- app.py
+++ app.py
@@ -13,35 +13,62 @@ bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
 
-
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-   # retrieve the message in JSON and then transform it to Telegram object
-   update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
-
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
    
-   
-   while text != "/stop":
-        # clear the message we got from any non alphabets
-        text = re.sub(r"\W", "_", text)
-        # create the api link for the avatar based on http://avatars.adorable.io/
-        url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-        # reply with a photo to the name the user sent,
-        # note that you can send photos by url and telegram will fetch it for you
-        bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
 
-   if text == "/echo" :
-       while text != "/stop":
-         update.message.reply_text(update.message.text)
-           
-   
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    # for debugging purposes only
+    print("got text message :", text)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
