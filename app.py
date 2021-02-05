5dd359a5e324150711c72ef2fe9c3ef2f70bbf68
Diogo Silva <diogovicente10@hotmail.com>
2021-02-05 10:26:56 +0000

    first commit

--- app.py
+++ app.py
@@ -29,15 +29,8 @@ def respond():
    text = update.message.text.encode('utf-8').decode()
    # for debugging purposes only
    print("got text message :", text)
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """
-       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-       """
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-   elif text == "/photo":
+   
+   if text == "/photo":
        try:
             # clear the message we got from any non alphabets
             text = re.sub(r"\W", "_", text)
