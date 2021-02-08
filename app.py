3089ef35360ccf7eb7166f2c3c42830b9cf02bb7
Diogo Silva <diogovicente10@hotmail.com>
2021-02-08 11:46:22 +0000

    first commit

--- app.py
+++ app.py
@@ -36,9 +36,13 @@ def respond():
 
    else:
        try:
-           # clear the message we got from any non alphabets
+         
            text = re.sub(r"\W", "_", text)
-           bot.sendMessage(chat_id=chat_id, text=text)
+           
+           if text == "/eco"
+                text = re.sub(r"\W", "_", text)
+                bot.sendMessage(chat_id=chat_id, text=bot_welcome)
+
        except Exception:
            # if things went wrong
            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
