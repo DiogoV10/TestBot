96fb72cdf4e56b5b98bceb3a8642a0a6ea29c9f5
Diogo Silva <diogovicente10@hotmail.com>
2021-02-08 13:03:55 +0000

    first commit

--- app.py
+++ app.py
@@ -26,13 +26,13 @@ def respond():
    print("got text message :", text)
    # the first time you chat with the bot AKA the welcoming message
 @bot.message_handler(commands=['start'])
-   def handle_command(message):
-        bot.reply_to(message, "Hello, welcome to Telegram Bot!")
+def handle_command(message):
+    bot.reply_to(message, "Hello, welcome to Telegram Bot!")
     
    # handle all messages, echo response back to users
 @bot.message_handler(func=lambda message: True)
-   def handle_all_message(message):
-	    bot.sendMessage(chat_id=chat_id, text=text)
+def handle_all_message(message):
+	bot.sendMessage(chat_id=chat_id, text=text)
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
