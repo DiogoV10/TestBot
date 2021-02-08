45257ad8c775dd9fafaec26b246565828c1f6a2c
Diogo Silva <diogovicente10@hotmail.com>
2021-02-08 12:38:13 +0000

    first commit

--- app.py
+++ app.py
@@ -11,53 +11,14 @@ bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
 
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-
-def respond():
-   # retrieve the message in JSON and then transform it to Telegram object
-   update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
-
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """No."""
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+@bot.message_handler(commands=['start'])
+def handle_command(message):
+    bot.reply_to(message, "Hello, welcome to Telegram Bot!")
+    
+# handle all messages, echo response back to users
+@bot.message_handler(func=lambda message: True)
+def handle_all_message(message):
+	bot.reply_to(message, message.text)
 
 
-   else:
-       try:
-         
-           
-           if text == "/echo":
-                while text != "/stop":
-                    bot.sendMessage(chat_id=chat_id, text=text)
-
-       except Exception:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
-
-   return 'ok'
-
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-   if s:
-       return "webhook setup ok"
-   else:
-       return "webhook setup failed"
-
-@app.route('/')
-def index():
-   return '.'
-
-
-if __name__ == '__main__':
-   app.run(threaded=True)
+bot.polling()
--- telebot/ai.py
+++
@@ -1,3 +0,0 @@
-def echo(text):
-    while text != "/stop":
-        bot.sendMessage(chat_id=chat_id, text=text)
