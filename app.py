6303586d283f8bf39ff54c53e739413a83276a4d
Diogo Silva <diogovicente10@hotmail.com>
2021-02-08 12:20:55 +0000

    first commit

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def respond():
          
            
            if text == "/echo":
-                echo(text)
+                while text != "/stop":
+                bot.sendMessage(chat_id=chat_id, text=text)
 
        except Exception:
            # if things went wrong
