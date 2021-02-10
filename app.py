24019845cfba720a0925454eeb764801275858e8
Diogo Silva <diogovicente10@hotmail.com>
2021-02-10 12:20:07 +0000

    first commit

--- app.py
+++ app.py
@@ -37,15 +37,14 @@ def respond():
    else:
        try:
            if text == "/echo":
-               bot.sendMessage(chat_id=chat_id, text="Do /echo [sample]")
+                bot.sendMessage(chat_id=chat_id, text="Do /echo [sample]")
            elif text.startswith("/echo "):  
                 user = text[6:]
                 bot.sendMessage(chat_id=chat_id, text=user) 
            elif text == "/":
+                
 
-
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
+           
 
        except Exception:
            # if things went wrong
