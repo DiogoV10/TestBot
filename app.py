62a07a8f4caced638b17d7b943ec0d6345f012e2
Diogo Silva <diogovicente10@hotmail.com>
2021-02-10 13:21:18 +0000

    first commit

--- app.py
+++ app.py
@@ -44,10 +44,10 @@ def respond():
                 bot.sendMessage(chat_id=chat_id, text=user)
 
            if text == "/greet":
-                markup = telebot.types.InlineKeyboardMarkup()
-                markup.add(telebot.types.InlineKeyboardButton(text='Bom dia', callback_data=1))
-                markup.add(telebot.types.InlineKeyboardButton(text='Boa tarde', callback_data=2))
-                markup.add(telebot.types.InlineKeyboardButton(text='Boa noite', callback_data=3))
+                markup = InlineKeyboardMarkup()
+                InlineKeyboardButton(text='Bom dia', callback_data=1)
+                InlineKeyboardButton(text='Boa tarde', callback_data=2)
+                InlineKeyboardButton(text='Boa noite', callback_data=3)
 
                 bot.send_message(message.chat.id, text="Choose your greeting:", reply_markup=markup)
 
